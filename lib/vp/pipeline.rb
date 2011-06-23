88053407a59cbb5b1267cf31eb6b7c7afeaff5ff
Jim V <jfv@stowers.org>
2011-06-23 15:30:25 -0500

    developing dsl for steps

--- lib/vp/pipeline.rb
+++ lib/vp/pipeline.rb
@@ -1,4 +1,64 @@
+class Step
+  attr_reader :name, :inputs, :outputs
+  attr_accessor :run_block
+  def initialize name
+    @name = name
+    @inputs = []
+    @outputs = []
+    @output_blocks = {}
+    @run_block = nil
+  end
+
+  def input value
+    @inputs << value.to_sym
+  end
+
+  def output value, &block
+    value = value.to_sym
+    @outputs << value
+    @output_blocks[value] = block if block
+  end
+
+  def outputs_given inputs
+    @output_values = {}
+    @outputs.each do |output|
+      @output_values[output] = @output_blocks[output].call(inputs) if @output_blocks[output]
+    end
+    @output_values
+  end
+
+  def run(inputs, outputs)
+    @run_block.call(inputs, outputs) if @run_block
+  end
+end
+
 class Pipeline
+
+  def self.step name, &block
+    @current_step = Step.new(name)
+    block.call
+  end
+
+  def self.input name
+    @current_step.input name
+  end
+
+  def self.output name, &block
+    @current_step.output(name, &block)
+  end
+
+  def self.run &run_block
+    @current_step.run_block = run_block
+    @@steps ||= []
+    @@steps << @current_step
+    @current_step = nil
+  end
+
+  def initialize
+    @steps = []
+    @current_step = nil
+  end
+
   def run options
     check_steps options[:steps]
 
