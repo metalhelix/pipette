7e55145fb8adc80c2217215e50ef9cdd752ed363
Jim V <jfv@stowers.org>
2011-09-02 13:40:16 -0500

    dummy pipeline

---
+++ lib/vp/pipelines/dummy_pipeline.rb
@@ -0,0 +1,20 @@
+require 'vp/pipeline'
+
+class DummyPipeline < Pipeline
+  options do
+    opts = OptionParser.new do |o|
+      o.on('-i', '--input BAM_FILE', 'REQUIRED - Input BAM file to call SNPs on') {|b| options[:input] = b}
+      o.on('-r', '--reference FA_FILE', 'REQUIRED - Reference Fasta file for genome') {|b| options[:reference] = b}
+    end
+    opts
+  end
+
+  step :print_input do
+    run do |input, output|
+      puts "input:"
+      puts input.inspect
+      puts "output:"
+      puts output.inspect
+    end
+  end
+end
