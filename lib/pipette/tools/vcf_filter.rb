aacc86746e1b6100ff19663878ce40cec7364cf0
Jim V <jfv@stowers.org>
2011-11-17 13:49:14 -0600

    disable het filtering

--- lib/pipette/tools/vcf_filter.rb
+++ lib/pipette/tools/vcf_filter.rb
@@ -6,11 +6,11 @@ class VCFFilter
     vcf = VCF.new input
     vcf.copy_if(output) do |vcf_hash| 
       rtn = vcf_hash["DP2"] > 10 
-      if rtn && vcf_hash["genotype"] == "isHet"
-        val  = (vcf_hash["AD"][1].to_f / vcf_hash["AD"][0].to_f)
-        rtn = val > 2.0
+      #if rtn && vcf_hash["genotype"] == "isHet"
+      #  val  = (vcf_hash["AD"][1].to_f / vcf_hash["AD"][0].to_f)
+      #  rtn = val > 2.0
         #puts "low divide: #{val}" unless rtn
-      end
+      #end
       rtn
     end
     vcf.close
