a4ce2c1d5d97eb1454915914d565285a298df74d
Jim V <jfv@stowers.org>
2012-04-03 15:32:27 -0500

    add comments

--- pipette.rb
+++ pipette.rb
@@ -8,8 +8,11 @@ ALL_PIPELINES = { "variant" => {:name => "variant pipeline", :class => VariantPi
                   "pe_bwa" => {:name => "paired-end bwa pipeline", :class => BwaPipeline}
                 }
 
+# Get the first argument. Shift so
+# pipelines don't receive it
 input_pipeline_name = ARGV.shift
 
+# Fail if no argument is provided
 if !input_pipeline_name
   puts "ERROR: No pipeline name provided"
   puts "please run using ./pipette.rb <pipeline_name>"
@@ -18,8 +21,10 @@ if !input_pipeline_name
   exit
 end
 
+# Acquire pipeline data for that name
 pipeline_data = ALL_PIPELINES[input_pipeline_name]
 
+# Fail if not a valid pipeline name
 if !pipeline_data
   puts "#{input_pipeline_name} not a valid pipeline"
   puts "Valid pipeline names are:"
@@ -29,8 +34,11 @@ end
 
 puts "running #{pipeline_data[:name]}"
 
+# Pull out the pipeline from the pipeline class
 pipeline = pipeline_data[:class].pipeline
 
+# Rest of the code below should probably be moved
+# to inside pipette framework
 pipeline.default_options
 pipeline.parse_input(ARGV)
 
