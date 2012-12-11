8bf3e6dd3a0e3e42181f66dbbf0734e96dd6529e
Jim Vallandingham <jfv@stowers.org>
2012-12-11 10:57:27 -0600

    add basic filters back

--- lib/pipette/tools/vcf_filter.rb
+++ lib/pipette/tools/vcf_filter.rb
@@ -5,12 +5,13 @@ class VCFFilter
   def self.filter input, output
     vcf = VCF.new input
     vcf.copy_if(output) do |vcf_hash| 
-      rtn = vcf_hash["DP2"] > 10 
-      #if rtn && vcf_hash["genotype"] == "isHet"
+      rtn = vcf_hash["DP2"] > 10
+      rtn &= vcf_hash["genotype"] != "isHet"
+      # if rtn && vcf_hash["genotype"] == "isHet"
       #  val  = (vcf_hash["AD"][1].to_f / vcf_hash["AD"][0].to_f)
       #  rtn = val > 2.0
         #puts "low divide: #{val}" unless rtn
-      #end
+      # end
       rtn
     end
     output
