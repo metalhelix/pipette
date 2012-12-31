d7351f6847a1d109875cf343b73474a89a1e2eb4
Jim Vallandingham <jfv@stowers.org>
2012-12-31 14:10:26 -0600

    add filter low quality

--- bulk/filter.rb
+++ bulk/filter.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/env ruby
 
 
-FILTER = File.expand_path(File.join(File.dirname(__FILE__), "pipette", "bin", "fast_filter_matching_vcfs.rb"))
+FILTER_MATCH = File.expand_path(File.join(File.dirname(__FILE__), "..", "bin", "fast_filter_matching_vcfs.rb"))
 
-ANNOTATE = File.expand_path(File.join(File.dirname(__FILE__), "pipette", "bin", "annotate_vcf.rb"))
+FILTER = File.expand_path(File.join(File.dirname(__FILE__), "..", "bin", "filter_vcf.rb"))
+
+ANNOTATE = File.expand_path(File.join(File.dirname(__FILE__), "..", "bin", "annotate_vcf.rb"))
 
 ANNOTATE_CONFIG = File.expand_path(File.join(File.dirname(__FILE__), "variant_config.yml"))
 
-LINKS = File.expand_path(File.join(File.dirname(__FILE__), "pipette", "bin", "add_igv_links.rb"))
+LINKS = File.expand_path(File.join(File.dirname(__FILE__), "..", "bin", "add_igv_links.rb"))
 
 INDEX_VCF = "java -Xmx1500m -Djava.awt.headless=true -jar /n/local/stage/igv_tools/current/igvtools.jar index"
 
@@ -17,9 +19,25 @@ starting_dir = ARGV[0]
 
 control_name = "Control"
 
-@control_snp_vcf_filename = File.expand_path(File.join(starting_dir, control_name, "#{control_name}.snps.vcf"))
-@control_indel_vcf_filename = File.expand_path(File.join(starting_dir, control_name, "#{control_name}.indels.vcf"))
 
+
+def filter_matching_vcfs vcf_files, control_name, starting_dir
+  control_snp_vcf_filename = File.expand_path(File.join(starting_dir, control_name, "#{control_name}.snps.vcf"))
+  control_indel_vcf_filename = File.expand_path(File.join(starting_dir, control_name, "#{control_name}.indels.vcf"))
+  vcf_files.each do |vcf_file|
+    puts vcf_file
+    dir_name = File.dirname(vcf_file)
+    Dir.chdir(dir_name) do
+      vcf_basename = File.basename(vcf_file)
+
+      filter_file = vcf_basename.split(".")[1] == "indels" ? control_indel_vcf_filename : control_snp_vcf_filename
+
+      command = "#{FILTER_MATCH} #{vcf_basename} #{filter_file}"
+      puts command
+      system(command)
+    end
+  end
+end
 
 def filter_vcfs vcf_files
   vcf_files.each do |vcf_file|
@@ -28,12 +46,11 @@ def filter_vcfs vcf_files
     Dir.chdir(dir_name) do
       vcf_basename = File.basename(vcf_file)
 
-      filter_file = vcf_basename.split(".")[1] == "indels" ? @control_indel_vcf_filename : @control_snp_vcf_filename
+      filter_name = File.basename(vcf_file, File.extname(vcf_file)) + ".filter.vcf"
 
-      command = "#{FILTER} #{vcf_basename} #{filter_file}"
+      command = "#{FILTER} #{vcf_basename} #{filter_name}"
       puts command
-        system(command)
-
+      system(command)
     end
   end
 end
@@ -87,25 +104,29 @@ def index_vcf_files vcf_files
 end
 
 vcf_files = Dir.glob(File.join(starting_dir, "**", "*.vcf"))
-#filter_vcfs vcf_files
+filter_matching_vcfs vcf_files, control_name, starting_dir
 
 vcf_files = Dir.glob(File.join(starting_dir, "**", "*.unique_from_#{control_name}.*.vcf"))
-#annotate_vcfs vcf_files
+filter_vcfs(vcf_files)
+
+vcf_files = Dir.glob(File.join(starting_dir, "**", "*.unique_from_#{control_name}.*.filter.vcf"))
+annotate_vcfs vcf_files
+
 output_dir = File.join("results", "vcf_files")
 copy_results vcf_files, output_dir
 
 vcf_files = Dir.glob(File.join(output_dir, "*.vcf"))
 index_vcf_files vcf_files
 
-tsv_files = Dir.glob(File.join(starting_dir, "**", "*.collapse.txt"))
-#add_links(tsv_files)
+tsv_files = Dir.glob(File.join(starting_dir, "**", "*filter*.collapse.txt"))
+add_links(tsv_files)
 
-result_files = Dir.glob(File.join(starting_dir, "**", "*.links.xls"))
+result_files = Dir.glob(File.join(starting_dir, "**", "*filter*.links.xls"))
 output_dir = File.join("results", "linked_files")
 copy_results result_files, output_dir
 
 output_dir = File.join("results", "summary_files")
-result_files = Dir.glob(File.join(starting_dir, "**", "*.snpeff.summary.html"))
+result_files = Dir.glob(File.join(starting_dir, "**", "*filter*.snpeff.summary.html"))
 copy_results result_files, output_dir
 
 
